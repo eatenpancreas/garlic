0fdb0825da07309e3ec39852d0061ae3cf7c8df4
Pascal d'Achard <0309597@student.rocvantwente.nl>
2025-02-27 04:39:33 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+use poem::{listener::TcpListener, Route, Server};
+use poem_openapi::{payload::Json, Object, OpenApi, OpenApiService};
+
+#[derive(Object)]
+struct User {
+    username: String,
+}
+
+struct Api;
+
+#[OpenApi]
+impl Api {
+    /// Hello world
+    #[oai(path = "/", method = "get")]
+    async fn index(&self) -> Json<&'static str> {
+        Json("Hello World")
+    }
+
+    /// Joe (test)
+    #[oai(path = "/joe", method = "get")]
+    async fn joe(&self) -> Json<User> {
+        Json(User {
+            username: "Joe".to_owned(),
+        })
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let api_service = OpenApiService::new(Api, "Hello World", env!("CARGO_PKG_VERSION"))
+        .server("http://localhost:3000");
+
+    let ui = api_service.swagger_ui();
+    let app = Route::new().nest("/", api_service).nest("/docs", ui);
+
+    Server::new(TcpListener::bind("127.0.0.1:3000"))
+        .run(app)
+        .await
+        .unwrap();
+}
